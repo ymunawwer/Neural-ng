93db0fe803354feb98723ebd296566fab2e53760
sasikumar <sasi@gamasome.com>
2018-08-31 00:06:43 +0530

    code changes for model count

--- controllers/preprocessModeling.js
+++ controllers/preprocessModeling.js
@@ -8,16 +8,13 @@ const neuralZomeUserModel = require(MODELS + 'neuralZomeUser');
 
 exports.getDetailsForStep3 = function (req, res, next) {
     try {
-        neuralZomeUserModel.update({
+        neuralZomeUserModel.findOneAndUpdate({
             email: req.body.email,
             "model.model_id": req.body.modelId
         }, {
-                '$set': {
-                    'model.$.algo': req.body.algo,
-                    'model.$.x_list': JSON.stringify(req.body.x_list),
-                    'model.$.y_list': JSON.stringify(req.body.y_list),
-                    'model.$.split_ratio': req.body.tt_split
-                }
+                'model.$.algo': req.body.algo,
+                'model.$.x_list': JSON.stringify(req.body.x_list),
+                'model.$.y_list': JSON.stringify(req.body.y_list),
             }, { multi: true }, function (err, record) {
                 if (err) {
                     console.log(err);
@@ -36,26 +33,25 @@ exports.getDetailsForStep3 = function (req, res, next) {
                             if (body.body.status == 'true') {
                                 var result = body.body;
                                 console.log(result);
-                                neuralZomeUserModel.update({
+                                neuralZomeUserModel.findOneAndUpdate({
                                     email: result.email,
                                     "model.model_id": result.modelId
                                 }, {
-                                        '$set': {
-                                            'model.$.accuracy': result.accuracy,
-                                            'model.$.train_split': result.train_split,
-                                            'model.$.test_split': result.test_split,
-                                            'model.$.model_file_path': result.download,
-                                            'model.$.pkl_file_path': result.pklfile,
-                                            'model.$.hint': result.hint
-                                        }
+                                        'model.$.accuracy': result.accuracy,
+                                        'model.$.train_split': result.train_split,
+                                        'model.$.test_split': result.test_split,
+                                        'model.$.model_file_path': result.download,
+                                        'model.$.pkl_file_path': result.pklfile,
+                                        'model.$.hint': result.hint,
+                                        $inc: { 'model.$.model_count': 1 }
                                     }, { multi: true }, function (err, record) {
                                         if (err) {
                                             console.log(err);
                                         } else {
                                             console.log(record);
+                                            res.sendResponse(result, 'Preprocessing successfully.');
                                         }
                                     });
-                                res.sendResponse(result, 'Preprocessing successfully.');
                             } else {
                                 next(body.body);
                             }
@@ -82,18 +78,11 @@ exports.getDetailsForStep4 = function (req, res, next) {
             } else {
                 if (body.body.status == 'true') {
                     var result = body.body;
-                    console.log(result);
-                    // neuralZomeUserModel.update({
+                    // neuralZomeUserModel.findOneAndUpdate({
                     //     email: result.email,
-                    //     "model.model_id": parseInt(result.modelId[0])
+                    //     "model.model_id": result.modelId
                     // }, {
-                    //         '$set': {
-                    //             'model.$.accuracy': result.accuracy,
-                    //             'model.$.train_split': result.train_split,
-                    //             'model.$.test_split': result.test_split,
-                    //             'model.$.model_file_path': result.download,
-                    //             'model.$.pkl_file_path': result.pklfile,
-                    //         }
+                    //         $inc: { 'model.$.api_hit_count': 1 }
                     //     }, { multi: true }, function (err, record) {
                     //         if (err) {
                     //             console.log(err);
